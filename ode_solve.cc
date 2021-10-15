91bf41eb897f7aef164c28ded29b4a2ccafb068a
Chris Lawrie <chrislawrie@dynamic-oit-vapornet100-10-9-41-125.princeton.edu>
2021-10-14 20:04:41 -0400

    started on ODE solver

---
+++ ode_solve.cc
@@ -0,0 +1,16 @@
+// General driver code for a given choice of ODE (eg. ddo, LV2)
+// and a given choice of solver (Euler, ddo, ab2)
+
+// *** how to check and read in and parse command line inputs?
+// *** how to print in c++? LMAO :D 
+
+const int num_args = 11;   // Expected number of command line arguments
+
+
+
+
+// Function to print out values 
+void print_values(double t, double x, double dx)
+{
+    //printf("%15.8f %15.8f %15.8f\n", t.c_str(), x.c_str(), dx.c_str());   // Prints time, x, dx
+}
