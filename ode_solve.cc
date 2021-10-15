bd880ff4284f9fd28641fb2fc0a07b694a2edb6b
Chris Lawrie <chrislawrie@chriss-air.myfiosgateway.com>
2021-10-15 09:34:30 -0400

    added content to ode_solve

--- ode_solve.cc
+++ ode_solve.cc
@@ -1,9 +1,11 @@
 // General driver code for a given choice of ODE (eg. ddo, LV2)
 // and a given choice of solver (Euler, ddo, ab2)
-
 // *** how to check and read in and parse command line inputs?
 // *** how to print in c++? LMAO :D 
 
+#include "ddo.h"
+#include "lv.h"
+#include "rk4.h"
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -33,7 +35,7 @@ double* string2double_array(string input)
 
     // Pass vector numbers into an array
     int n = nums.size(); 
-    double[] output = new double[n];
+    double* output = new double[n];
 
     for (int i = 0; i < n; i++) 
     {
@@ -42,8 +44,6 @@ double* string2double_array(string input)
 
     return output; 
 }
-
-
 
 
 int main(int argc, char *argv[])
@@ -59,32 +59,51 @@ int main(int argc, char *argv[])
     // Which model do we want?
     string model = argv[1];   
 
-    // What should we set our parameters to?
-    string params_str = argv[2];
-    double[] params = string2double_array(params_str);    // "4.0 5.0 6.0 7.0"
+    // What should we set our model parameters to?
+    string mod_params_str = argv[2];
+    double [] mod_params = string2double_array(mod_params_str); 
+
+    // What should we set our initial conditions to?
+    string i_conds_str = argv[3];
+    double [] i_conds = string2double_array(i_conds_str); 
+
+    // Which solver do we want?
+    string solver = argv[4];   
+
+    // Whats the time step?
+    double time_step = stod(argv[5]);   
+
+    // How many time steps should we take?
+    int tot_steps = stoi(argv[6]);   
 
 
 
+    // Print initial conditions
+    print_values(t, dx[0], dx[1]);
 
 
+    // Figure out what Model we want
+    Model system_model;
+
+    // Define our model :)
+    if (model == "ddo") system_model = new DDOscillator(mod_params);
+    else if (model == "lv") system_model = new LotkaVolterra(mod_params);
 
 
-    // // Creating and initialising a VLA of size n for solving system
-    // double dx[n];
-    // for (int i = 0; i < n; i++) dx[i] = 0;
+    // Figure out what integrator we want
+    Integrator system_solver;  
+    if (solver == "Euler")    system_solver = new Euler(time_step, system_model); 
+    else if (solver == "ab2") system_solver = new AdamsBashforth2(time_step, system_model);
+    else if (solver == "rk4") system_solver = new RungeKutta4(time_step, system_model);
 
-    // // Create a pointer to the new integrator and assigns memory for the integrator object
-    // Integrator *solver = integrator_new(n, dt, &rhs);
 
-    // // Print initial conditions
-    // print_values(t, dx[0], dx[1]);
+    // For each time step, solve the system and iterate
+    for (int i = 0; i < tot_steps; i++)
+    {
+      //  t += dt;
+        print_values(t, dx[0], dx[1]);
+    }
 
-    // // For each time step, solve the system and iterate
-    // for (int i = 0; i < N; i++)
-    // {
-    //     integrator_step(solver, t, dx);
-    //     t += dt;
-    //     print_values(t, dx[0], dx[1]);
-    // }
-    // integrator_free(solver);
+    delete [] mod_params;
+    delete [] i_conds;
 }
